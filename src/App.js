54fafc96fdb3744f22e428ae450e72791fd5a2e8
mertoz41 <mertozka41@gmail.com>
2021-08-27 14:01:23 +0300

    navigation to waitlist implemented"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
 import FullScreen from './fullScreen';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Waitlist from './components/waitlist'
 function App() {
 
   return (
-    <div>
-      <FullScreen />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" render={() => <FullScreen/>}/>
+  
+        <Route exact path="/signup" render={() => <Waitlist/>}/>
+          
+        
+      </Switch>
+    </Router>
   );
 }
 
--- src/components/last.js
+++ src/components/last.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import { withRouter } from "react-router-dom";
 
-const Last = () => {
+// import { useNavigate } from "react-router-dom";
+// const history = useNavigate()
+const Last = ({history}) => {
+    // console.log(history)
+    // const history = useNavigate()
     return(
         <div>
             <h4>EXPERIMENTING THAT WAITLIST BUTTON</h4>
-            <button>to waitlist</button>
+            <button onClick={() => history.push('/signup')}>to waitlist</button>
         </div>
     )
 }
 
-export default Last
+export default withRouter(Last)
