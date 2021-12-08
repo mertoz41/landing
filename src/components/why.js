52a34264bfd4aca7de1f4cb36708bdbb3277bb63
bob <mertozka41@gmail.com>
2021-12-08 00:52:01 -0500

    oh boy

--- src/components/why.js
+++ src/components/why.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import '../App.css'
 import { BsFillCaretDownFill } from "react-icons/bs";
+import Waves from '../pics/youwaves.jpg'
 
 const What = ({handlePageChange, currentPage}) => {
     return(
         <div className='container'>
             <div className="logobox">
                 <h1>Why Cezs?</h1>
-            </div>
-            <p>
+                <p>
                 MUSICIANS PLATFORMS ARE NOT CONVENIENT FOR INSTRUMENTALISTS AND THEYRE ONLY USED BY INDIVIDUALS LOOKING TO MAKE IT.
                 SOCIAL MEDIA PLATFORMS ARE TOO VAST
                 MUSIC NEEDS ITS OWN SPACE. 
@@ -21,6 +21,10 @@ const What = ({handlePageChange, currentPage}) => {
                 While useful for some, instrumentalists are underrepresented in these platforms as their talents are indistinguishable behind sound files, which require some degree of production, overcomplicating sharing music. 
                 Worse, these platforms are only used by aspiring artists alike. 
             </p>
+            </div>
+            <img src={Waves} alt='omerss' style={{height: 100, justifyContent: 'center', width: '100%'}}/>
+
+            
             <div className='nextpage' onClick={() => handlePageChange(currentPage + 1)}>
             <BsFillCaretDownFill size='3em' className="downbutton"/>
 
